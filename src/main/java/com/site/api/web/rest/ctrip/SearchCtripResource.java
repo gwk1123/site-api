ce91a2f21b8b4cb44fd263fc686c7dbadabba234
gwk1123 <g15625281551@163.com>
2020-10-04 21:31:05 +0800

    重构==

--- src/main/java/com/site/api/web/rest/ctrip/SearchCtripResource.java
+++ src/main/java/com/site/api/web/rest/ctrip/SearchCtripResource.java
@@ -14,12 +14,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/flight")
+@Controller
+@RequestMapping(value = "/api")
 public class SearchCtripResource {
 
     private Logger logger = LoggerFactory.getLogger(SearchCtripResource.class);
@@ -30,9 +29,8 @@ public class SearchCtripResource {
     @Autowired
     private SibeSearchService sibeSearchService;
 
-    @RequestMapping(value = "/search", method = RequestMethod.POST,
-            consumes = MediaType.TEXT_PLAIN_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    @RequestMapping(value = "/search")
     public String searchCtrip(@RequestBody String request) throws Exception {
         CtripSearchRequest ctripSearchRequest = null;
         try {
